7a7f11eba3a94a0e3060f79471c80cd4655484d7
dbc-apprentice <apprentice@devbootcamp.com>
2013-11-04 14:00:37 -0600

    commented some of the code

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   has_many :karma_points
 
-  attr_accessible :first_name, :last_name, :email, :username, :point_total
+  attr_accessible :first_name, :last_name, :email, :username, :point_total # add point_total as a reader to access the variable (when seeding)
 
   validates :first_name, :presence => true
   validates :last_name, :presence => true
@@ -18,16 +18,16 @@ class User < ActiveRecord::Base
             :uniqueness => {:case_sensitive => false}
 
   def self.by_karma
-    User.order("point_total DESC")
+    User.order("point_total DESC") #modified this to reflect the indexing (made the read much quicker vs. going through all database fields)
     # joins(:karma_points).group('users.id').order('SUM(karma_points.value) DESC')
   end
 
   def sum_karma
-    self.karma_points.sum(:value)
+    self.karma_points.sum(:value) #used this to seed the new field - after that, it's kind of done
   end
 
   def total_karma
-    self.point_total
+    self.point_total # added this as a new method
   end
 
   def full_name
